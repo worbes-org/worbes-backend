022fc6ca027d5a1803a4df0b5b6fd2d31e363e56
정용우 <jyw1341@gmail.com>
2024-11-25 20:14:42 +0900

    feat: MySQL 데이터베이스 설정 추가 및 기본 애플리케이션 구조 구성

---
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/AuctionHouseTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.worbes.auctionhousetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionHouseTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionHouseTrackerApplication.class, args);
+    }
+}
