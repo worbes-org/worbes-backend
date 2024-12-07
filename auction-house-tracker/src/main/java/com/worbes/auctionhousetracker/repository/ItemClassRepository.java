c4727f736ed65378718edfe3279dbe4115d1bac0
정용우 <jyw1341@gmail.com>
2024-12-07 17:41:18 +0900

    feat: ItemClass 엔티티에 대한 CRUD 작업을 위한 ItemClassRepository 인터페이스 추가

---
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/repository/ItemClassRepository.java
@@ -0,0 +1,7 @@
+package com.worbes.auctionhousetracker.repository;
+
+import com.worbes.auctionhousetracker.entity.ItemClass;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemClassRepository extends JpaRepository<ItemClass, Long> {
+}
