2516d967ae4e666675df0f45259bec920f3f6987
정용우 <jyw1341@gmail.com>
2024-12-07 19:08:10 +0900

    feat: ItemSubclassRepository 인터페이스 추가

---
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/repository/ItemSubclassRepository.java
@@ -0,0 +1,7 @@
+package com.worbes.auctionhousetracker.repository;
+
+import com.worbes.auctionhousetracker.entity.ItemSubclass;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemSubclassRepository extends JpaRepository<ItemSubclass, Long> {
+}
