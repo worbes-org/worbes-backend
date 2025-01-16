4d3d03ca3546152d3b7d9130a423ccd9fa7cc2fb
정용우 <jyw1341@gmail.com>
2025-01-16 11:20:25 +0900

    feat: 아이템 서브 클래스 상세 정보 DTO 생성
    - From Item Subclass API Response Body

---
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/dto/response/ItemSubclassDetailResponse.java
@@ -0,0 +1,21 @@
+package com.worbes.auctionhousetracker.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.worbes.auctionhousetracker.entity.embeded.Language;
+import lombok.Data;
+
+@Data
+public class ItemSubclassDetailResponse {
+
+    @JsonProperty("subclass_id")
+    private Long id;
+
+    @JsonProperty("class_id")
+    private Long classId;
+
+    @JsonProperty("display_name")
+    private Language displayName;
+
+    @JsonProperty("verbose_name")
+    private Language verboseName;
+}
