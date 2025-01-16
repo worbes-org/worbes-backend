584bd71dabfa1d5f8bcfc6cb5911479d0bf41ab8
정용우 <jyw1341@gmail.com>
2025-01-16 11:21:13 +0900

    feat: 아이템 클래스 응답 DTO 생성
    - From Item Class API Response Body

---
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/dto/response/ItemClassResponse.java
@@ -0,0 +1,19 @@
+package com.worbes.auctionhousetracker.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.worbes.auctionhousetracker.entity.embeded.Language;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ItemClassResponse {
+
+    @JsonProperty("class_id")
+    private Long id;
+
+    private Language name;
+
+    @JsonProperty("item_subclasses")
+    private List<ItemSubclassResponse> subclassResponses;
+}
