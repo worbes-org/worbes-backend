627be7a0eb14234a005aeb606c7b9ad55fc60b9d
정용우 <jyw1341@gmail.com>
2025-01-16 11:25:36 +0900

    feat: ItemSubclass 저장 작업을 위한 CommandLineRunner 추가 및 순서 지정

--- auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/config/DataConfig.java
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/config/DataConfig.java
@@ -1,15 +1,24 @@
 package com.worbes.auctionhousetracker.config;
 
 import com.worbes.auctionhousetracker.service.ItemClassService;
+import com.worbes.auctionhousetracker.service.ItemSubclassService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class DataConfig {
 
     @Bean
+    @Order(1)
     public CommandLineRunner itemClassDataLoader(ItemClassService itemClassService, BearerTokenHandler bearerTokenHandler) {
         return new ItemClassDataLoader(itemClassService, bearerTokenHandler);
+    }
+
+    @Bean
+    @Order(2)
+    public CommandLineRunner itemSubclassDataLoader(ItemClassService itemClassService, ItemSubclassService itemSubclassService, BearerTokenHandler bearerTokenHandler) {
+        return new ItemSubclassDataLoader(itemClassService, itemSubclassService, bearerTokenHandler);
     }
 }
