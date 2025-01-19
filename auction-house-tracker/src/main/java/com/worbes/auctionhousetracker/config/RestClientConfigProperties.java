e7adfc6bf13f7a4900ea40a29784e7769cfd3a95
정용우 <jyw1341@gmail.com>
2025-01-20 00:06:08 +0900

    feat: 블리자드 API 기본 URL 설정 클래스 추가

---
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/config/RestClientConfigProperties.java
@@ -0,0 +1,12 @@
+package com.worbes.auctionhousetracker.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "blizzard.api")
+@Getter
+@Setter
+public class RestClientConfigProperties {
+    private String baseUrl;
+}
