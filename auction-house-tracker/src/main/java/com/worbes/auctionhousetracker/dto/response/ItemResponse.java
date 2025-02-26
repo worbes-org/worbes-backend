45cd468b4562170cc12c49c8fcca04154d1ae53c
정용우 <jyw1341@gmail.com>
2025-02-26 17:01:35 +0900

    feat: Item 데이터 조회 DTO 구현

--- auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/dto/response/ItemResponse.java
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/dto/response/ItemResponse.java
@@ -2,8 +2,6 @@ package com.worbes.auctionhousetracker.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.worbes.auctionhousetracker.entity.embeded.Language;
-import com.worbes.auctionhousetracker.entity.enums.BindingType;
-import com.worbes.auctionhousetracker.entity.enums.InventoryType;
 import com.worbes.auctionhousetracker.entity.enums.Quality;
 import lombok.Data;
 
@@ -12,76 +10,33 @@ import java.util.Map;
 @Data
 public class ItemResponse {
 
+    private Long id;
+
+    private Language name;
+
+    private Quality quality;
+
+    private Long itemClassId;
+
+    private Long itemSubclassId;
+
+    private Integer level;
+
     @JsonProperty("preview_item")
-    private PreviewItem previewItem;
+    private Object previewItem;
 
-    @Data
-    private static class PreviewItem {
-
-        private Long id;
-        private Quality quality;
-        private Language name;
-        private String mediaHref;
-        private Long itemClassId;
-        private Long itemSubclassId;
-        private InventoryType inventoryType;
-        private Long sellPrice;
-        private Language description;
-        @JsonProperty("is_subclass_hidden")
-        private Boolean isSubclassHidden;
-        @JsonProperty("crafting_reagent")
-        private Language craftingReagent;
-        private BindingType bindingType;
-
-        @JsonProperty("item")
-        private void mapItemId(Map<String, Object> item) {
-            id = (Long) item.get("id");
-        }
-
-        @JsonProperty("quality")
-        private void mapQualityType(Map<String, Object> type) {
-            quality = Quality.valueOf((String) type.get("type"));
-        }
-
-        @JsonProperty("media")
-        private void mapMediaHref(MediaDto mediaDto) {
-            mediaHref = mediaDto.getKey().getHref();
-        }
-
-        @JsonProperty("item_class")
-        private void mapItemClassId(Map<String, Object> map) {
-            itemClassId = (Long) map.get("id");
-        }
-
-        @JsonProperty("item_subclass")
-        private void mapItemSubclassId(Map<String, Object> map) {
-            itemSubclassId = (Long) map.get("id");
-        }
-
-        @JsonProperty("inventory_type")
-        private void mapInventoryType(Map<String, Object> map) {
-            inventoryType = InventoryType.valueOf((String) map.get("type"));
-        }
-
-        @JsonProperty("sell_price")
-        private void mapSellPrice(Map<String, Object> map) {
-            sellPrice = (Long) map.get("value");
-        }
-
-        @JsonProperty("binding")
-        private void mapBindType(Map<String, Object> map) {
-            bindingType = BindingType.valueOf((String) map.get("type"));
-        }
+    @JsonProperty("quality")
+    private void unpackNestedQuality(Map<String, String> quality) {
+        this.quality = Quality.valueOf(quality.get("type"));
     }
 
-    @Data
-    public static class MediaDto {
-        private KeyDto key;
-        private Long id;
+    @JsonProperty("item_class")
+    private void unpackNestedItemClass(Map<String, String> itemClass) {
+        this.itemClassId = Long.valueOf(itemClass.get("id"));
     }
 
-    @Data
-    public static class KeyDto {
-        private String href;
+    @JsonProperty("item_subclass")
+    private void unpackNestedItemSubclass(Map<String, String> itemSubclass) {
+        this.itemSubclassId = Long.valueOf(itemSubclass.get("id"));
     }
 }
