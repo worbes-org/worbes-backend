ab0982379bf6bd940bfdba8eac3f941b5248ccec
정용우 <jyw1341@gmail.com>
2025-02-27 18:04:42 +0900

    feat: RealmIndexResponse 객체 추가

---
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/dto/response/RealmIndexResponse.java
@@ -0,0 +1,22 @@
+package com.worbes.auctionhousetracker.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.worbes.auctionhousetracker.entity.embeded.Language;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RealmIndexResponse {
+
+    private List<RealmIndexElement> realms;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class RealmIndexElement {
+        private Long id;
+        private Language name;
+        private String slug;
+    }
+}
