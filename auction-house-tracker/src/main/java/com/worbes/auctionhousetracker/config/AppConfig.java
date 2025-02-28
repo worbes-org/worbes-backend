be4d20a00a7240a0627c17420d2701240d851e6d
정용우 <jyw1341@gmail.com>
2025-02-28 22:50:42 +0900

    feat(config): 스레드 풀 TaskExecutor 및 TaskScheduler 빈 추가
    애플리케이션 비동기 작업과 스케줄링 기능을 지원하기 위해
    ThreadPoolTaskExecutor와 ThreadPoolTaskScheduler 빈을 추가함.

--- auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/config/AppConfig.java
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/config/AppConfig.java
@@ -4,6 +4,8 @@ import com.worbes.auctionhousetracker.config.properties.BlizzardApiConfigPropert
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableJpaAuditing
@@ -12,5 +14,23 @@ public class AppConfig {
     @Bean
     BlizzardApiConfigProperties blizzardApiConfigProperties() {
         return new BlizzardApiConfigProperties();
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);  // 스케줄링을 위한 스레드 개수
+        scheduler.setThreadNamePrefix("ScheduledTask-");
+        scheduler.initialize();
+        return scheduler;
     }
 }
