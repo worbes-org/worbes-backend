5576adb1589a4dec6c60566e760ec72218d53460
정용우 <jyw1341@gmail.com>
2025-02-28 22:51:59 +0900

    feat(entity): Realm 엔티티에 region 필드 추가 및 Builder 어노테이션 적용
    - Region enum 추가 및 EnumType.STRING으로 매핑
    - 기존 Builder 클래스 삭제 후 Lombok @Builder 어노테이션 적용

--- auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/entity/Realm.java
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/entity/Realm.java
@@ -1,12 +1,12 @@
 package com.worbes.auctionhousetracker.entity;
 
 import com.worbes.auctionhousetracker.entity.embeded.Language;
+import com.worbes.auctionhousetracker.entity.enums.Region;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
@@ -20,39 +20,15 @@ public class Realm {
     private Language name;
 
     private Long connectedRealmId;
-    
-    private Realm(Long id, Language name, Long connectedRealmId) {
+
+    @Enumerated(EnumType.STRING)
+    private Region region;
+
+    @Builder
+    private Realm(Long id, Language name, Long connectedRealmId, Region region) {
         this.id = id;
         this.name = name;
         this.connectedRealmId = connectedRealmId;
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static class Builder {
-        private Long id;
-        private Language name;
-        private Long connectedRealmId;
-
-        public Builder id(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public Builder name(Language name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder connectedRealmId(Long connectedRealmId) {
-            this.connectedRealmId = connectedRealmId;
-            return this;
-        }
-
-        public Realm build() {
-            return new Realm(id, name, connectedRealmId);
-        }
+        this.region = region;
     }
 }
