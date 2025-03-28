46e72118bc80325602821adaf070394829080fe3
정용우 <jyw1341@gmail.com>
2025-03-29 00:30:57 +0900

    feat: 아이템 서브클래스 조회 기능 구현
    - 아이템 클래스 + 아이템 서브 클래스 아이디로 조회

--- auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/repository/ItemSubclassRepository.java
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/repository/ItemSubclassRepository.java
@@ -1,15 +1,19 @@
 package com.worbes.auctionhousetracker.repository;
 
+import com.worbes.auctionhousetracker.entity.ItemClass;
 import com.worbes.auctionhousetracker.entity.ItemSubclass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface ItemSubclassRepository extends JpaRepository<ItemSubclass, Long> {
 
     @Query("SELECT s FROM ItemSubclass s JOIN FETCH s.itemClass WHERE s.itemClass.id IN :classIds")
     List<ItemSubclass> findByItemClassIdIn(@Param("classIds") Set<Long> classIds);
+
+    Optional<ItemSubclass> findByItemClassAndSubclassId(ItemClass itemClass, Long subclassId);
 }
--- auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/service/ItemSubclassService.java
+++ auction-house-tracker/src/main/java/com/worbes/auctionhousetracker/service/ItemSubclassService.java
@@ -1,12 +1,16 @@
 package com.worbes.auctionhousetracker.service;
 
 import com.worbes.auctionhousetracker.dto.response.ItemSubclassResponse;
+import com.worbes.auctionhousetracker.entity.ItemClass;
+import com.worbes.auctionhousetracker.entity.ItemSubclass;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public interface ItemSubclassService {
+    ItemSubclass get(ItemClass itemClass, Long itemSubclassId);
+
     void saveRequiredSubclass(List<ItemSubclassResponse> responses, Long itemClass);
 
     Map<Long, Set<Long>> getMissingItemSubclasses();
