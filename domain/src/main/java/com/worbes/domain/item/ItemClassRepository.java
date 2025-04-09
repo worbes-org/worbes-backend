a55cc3af61052620c4877199cc7de5cc393e98a7
정용우 <jyw1341@gmail.com>
2025-04-09 20:19:24 +0900

    feat(domain): ItemClassRepository findAllBy 인터페이스 추가

--- domain/src/main/java/com/worbes/domain/item/ItemClassRepository.java
+++ domain/src/main/java/com/worbes/domain/item/ItemClassRepository.java
@@ -1,9 +1,12 @@
 package com.worbes.domain.item;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItemClassRepository {
     Optional<ItemClass> findBy(Long id);
+
+    List<ItemClass> findAllBy(Iterable<Long> classIds);
 
     void save(ItemClass itemClass);
 
