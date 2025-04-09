1a31db76df15b475cbe7a880e3e4f846cc6ce026
정용우 <jyw1341@gmail.com>
2025-04-09 15:26:38 +0900

    feat: 퀄리티 타입 추가

---
+++ domain/src/main/java/com/worbes/domain/item/QualityType.java
@@ -0,0 +1,13 @@
+package com.worbes.domain.item;
+
+public enum QualityType {
+    POOR,
+    COMMON,
+    UNCOMMON,
+    RARE,
+    EPIC,
+    LEGENDARY,
+    ARTIFACT,
+    HEIRLOOM,
+    WOW_TOKEN
+}
