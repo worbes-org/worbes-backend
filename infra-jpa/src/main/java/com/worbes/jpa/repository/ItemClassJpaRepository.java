50057bda8f61aa76d041da45ea3608c97a21e540
정용우 <jyw1341@gmail.com>
2025-04-09 17:31:26 +0900

    feat(infra-jpa): 아이템 클래스 저장소 구현

---
+++ infra-jpa/src/main/java/com/worbes/jpa/repository/ItemClassJpaRepository.java
@@ -0,0 +1,7 @@
+package com.worbes.jpa.repository;
+
+import com.worbes.jpa.entity.ItemClassEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemClassJpaRepository extends JpaRepository<ItemClassEntity, Long> {
+}
