495418d1c504eec2043c5e93d809d73c09ad86db
정용우 <jyw1341@gmail.com>
2025-04-16 20:50:56 +0900

    feat: LocalizedName ItemClass에 적용

--- domain/src/main/java/com/worbes/domain/item/ItemClass.java
+++ domain/src/main/java/com/worbes/domain/item/ItemClass.java
@@ -1,34 +1,30 @@
 package com.worbes.domain.item;
 
-import lombok.Builder;
+import com.worbes.domain.shared.LocalizedName;
 import lombok.Getter;
 
-import java.util.Map;
-import java.util.Objects;
-
 @Getter
-@Builder
 public class ItemClass {
 
-    private Long id;
-    private Map<String, String> name;
+    private final Long id;
+    private final LocalizedName name;
 
-    public static ItemClass create(Long id, Map<String, String> name) {
-        return ItemClass.builder()
-                .id(id)
-                .name(name)
-                .build();
+    public ItemClass(Long id, LocalizedName name) {
+        if (id == null) throw new IllegalArgumentException("id는 null일 수 없습니다.");
+        if (name == null) throw new IllegalArgumentException("name은 null일 수 없습니다.");
+        this.id = id;
+        this.name = name;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         ItemClass itemClass = (ItemClass) o;
-        return Objects.equals(id, itemClass.id);
+        return id.equals(itemClass.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(id);
+        return id.hashCode();
     }
 }
