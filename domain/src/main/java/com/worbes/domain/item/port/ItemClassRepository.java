8af0e3c6c673fc67acc0c3e681b3ef7f4fbac9c9
정용우 <jyw1341@gmail.com>
2025-04-16 17:48:25 +0900

    feat(domain): 아이템 클래스 조회 기능 반환타입 Set으로 변경

--- domain/src/main/java/com/worbes/domain/item/port/ItemClassRepository.java
+++ domain/src/main/java/com/worbes/domain/item/port/ItemClassRepository.java
@@ -2,13 +2,13 @@ package com.worbes.domain.item.port;
 
 import com.worbes.domain.item.ItemClass;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface ItemClassRepository {
     Optional<ItemClass> findBy(Long id);
 
-    List<ItemClass> findAllBy(Iterable<Long> classIds);
+    Set<ItemClass> findAllBy(Iterable<Long> classIds);
 
     void save(ItemClass itemClass);
 
