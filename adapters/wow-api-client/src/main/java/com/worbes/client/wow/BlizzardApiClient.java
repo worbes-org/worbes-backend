3304472e436eb4419258ff6b425a38bbb3a8fb8c
정용우 <jyw1341@gmail.com>
2025-04-17 22:33:38 +0900

    feat:

--- adapters/wow-api-client/src/main/java/com/worbes/client/wow/BlizzardApiClient.java
+++ adapters/wow-api-client/src/main/java/com/worbes/client/wow/BlizzardApiClient.java
@@ -1,7 +1,23 @@
 package com.worbes.client.wow;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.Map;
 
-public interface BlizzardApiClient {
-    <T> T fetch(String url, Map<String, String> queryParams, Class<T> responseType);
+@RequiredArgsConstructor
+public class BlizzardApiClient {
+
+    private final RestApiClient client;
+    private final BlizzardOauthClient tokenHandler;
+
+    public <T> T fetch(String url, Map<String, String> queryParams, Class<T> responseType) {
+        String token = tokenHandler.get();
+        var requestParams = RequestParams.builder()
+                .url(url)
+                .queryParams(queryParams)
+                .headers(Map.of("Authorization", "Bearer " + token))
+                .build();
+
+        return client.get(requestParams, responseType);
+    }
 }
