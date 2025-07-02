32c60302b481c0eeda5c943f9efa31295d6572dd
정용우 <jyw1341@gmail.com>
2025-07-02 13:04:41 +0900

    경매 조회 api 응답 사양 변경
    - 기존 tier : FIRST
    - 변경 후 tier : 1

--- adapter/web/src/main/java/com/worbes/web/auction/model/SearchAuctionResponse.java
+++ adapter/web/src/main/java/com/worbes/web/auction/model/SearchAuctionResponse.java
@@ -1,7 +1,6 @@
 package com.worbes.web.auction.model;
 
 import com.worbes.application.auction.model.AuctionSummary;
-import com.worbes.application.item.model.CraftingTierType;
 import lombok.Data;
 
 import java.util.Map;
@@ -11,14 +10,14 @@ public class SearchAuctionResponse {
 
     private final Map<String, String> name;
     private final String iconUrl;
-    private final CraftingTierType tier;
+    private final Integer tier;
     private final Long available;
     private final PriceResponse lowestPrice;
 
     public SearchAuctionResponse(AuctionSummary auctionSummary) {
         name = auctionSummary.getItemName();
         iconUrl = auctionSummary.getIconUrl();
-        tier = auctionSummary.getCraftingTier();
+        tier = auctionSummary.getCraftingTier().getValue();
         lowestPrice = new PriceResponse(auctionSummary.getPrice());
         available = auctionSummary.getAvailable();
     }
--- application/src/main/java/com/worbes/application/item/model/CraftingTierType.java
+++ application/src/main/java/com/worbes/application/item/model/CraftingTierType.java
@@ -1,9 +1,27 @@
 package com.worbes.application.item.model;
 
+import lombok.Getter;
+
+@Getter
 public enum CraftingTierType {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH,
+    FIRST(1),
+    SECOND(2),
+    THIRD(3),
+    FOURTH(4),
+    FIFTH(5);
+
+    private final int value;
+
+    CraftingTierType(int value) {
+        this.value = value;
+    }
+
+    public static CraftingTierType fromValue(int value) {
+        for (CraftingTierType tier : values()) {
+            if (tier.value == value) {
+                return tier;
+            }
+        }
+        throw new IllegalArgumentException("Invalid tier: " + value);
+    }
 }
